6051b1101d8d6a30c0dfc751d90fa8f6e57b612b
Zach Munro-Cape <zach.munrocape@gmail.com>
2015-02-19 15:27:20 -0400

    Convert YAML to map

--- generate-json.go
+++ generate-json.go
@@ -1,41 +1,45 @@
 package main
- 
-import (
-    "fmt"
-    "net/http"
-    "io/ioutil"
-    "os"
 
-    "gopkg.in/yaml.v2"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	"gopkg.in/yaml.v2"
 )
 
-type Languages struct {
-	Languages []Language
-}
-
-type Language struct {
-
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
 
 func main() {
-    
-	// Get YAML representing all languages
-    response, err := http.Get("https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml")
-    var unmarshalled_yaml []byte
-    if err != nil {
-        fmt.Printf("%s\n", err)
-        os.Exit(1)
-    }
-    defer response.Body.Close()
-    contents, err := ioutil.ReadAll(response.Body)
-    if err != nil {
-        fmt.Printf("%s\n", err)
-        os.Exit(1)
-    }
-    unmarshalled_yaml = []byte(contents)
 
-    // Parse YAML into struct representation
-    
-    // Write to JSON
+	// Get YAML representing all languages
+	var unmarshalled_yaml []byte
+	response, err := http.Get("https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml")
+	check(err)
+	
+	defer response.Body.Close()
+	contents, err := ioutil.ReadAll(response.Body)
+	check(err)
+	
+	unmarshalled_yaml = []byte(contents)
+
+	// Parse YAML into map representation
+
+	m := make(map[interface{}]interface{})
+	err = yaml.Unmarshal(unmarshalled_yaml, &m)
+	check(err)
+
+	fmt.Printf("%#v\n", m)
+
+	// Convert map to JSON
+
+	// Write to file
+
 
 }
