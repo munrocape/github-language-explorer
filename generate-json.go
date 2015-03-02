0119cea2cc5ec9c224f0b9259c1598af3868aa3f
Zach Munro-Cape <zach.munrocape@gmail.com>
2015-03-01 20:53:43 -0400

    Bespoke sorting implemented for Language struct type. Conversion of hex to RGB and hue

--- generate-json.go
+++ generate-json.go
@@ -6,18 +6,38 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"sort"
 
 	"gopkg.in/yaml.v2"
 )
 
-type Language struct {
-	Name string
+type Color struct {
 	Hex string
-	R float64 // To find the hue, 1 / {R, G, B} is required
-	G float64 // Typical uint8 assignment is therefore not ideal
+	R float64
+	G float64
 	B float64
 	Hue float64
 
+}
+
+type Language struct {
+	Name string
+	Color Color
+}
+
+// Sorting implementation for language list
+type Languages []Language
+
+func (slice Languages) Len() int {
+	return len(slice)
+}
+
+func (slice Languages) Less(i, j int) bool {
+	return slice[i].Color.Hue < slice[j].Color.Hue	
+}
+
+func (slice Languages) Swap(i, j int) {
+	slice[i], slice[j] = slice[j], slice[i]
 }
 
 func check(e error) {
@@ -26,7 +46,27 @@ func check(e error) {
 	}
 }
 
-func hexToRGB(str string) (int64, int64, int64) {
+func ternaryMax(a float64, b float64, c float64) float64 {
+	if (a > b) && (a > c) {
+		return a
+	} else if (b > a) && (b > c) {
+		return b
+	} else {
+		return c
+	}
+}
+
+func ternaryMin(a float64, b float64, c float64) float64 {
+	if (a < b) && (a < c) {
+		return a
+	} else if (b < a) && (b < c) {
+		return b
+	} else {
+		return c
+	}
+}
+
+func hexToRGB(str string) (float64, float64, float64) {
 	var r, g, b int64
 	if(len(str) == 4){
 		r, _ = strconv.ParseInt(str[1:3], 16, 0)
@@ -37,13 +77,44 @@ func hexToRGB(str string) (int64, int64, int64) {
 		g, _ = strconv.ParseInt(str[3:5], 16, 0)
 		b, _ = strconv.ParseInt(str[5:7], 16, 0)
 	}
-	return r, g, b
+	return float64(r), float64(g), float64(b)
 }
 
-func createLangStruct(name string, hex string) *Language {
-	//r, g, b := hexToRGB(hex)
-	//fmt.Printf("lang: %s hex: %s rgb: %d %d %d\n", name, hex, r, g, b)
-	return new(Language)
+func hueFromRGB(r float64, g float64, b float64) float64 {
+	r = r / 255
+	g = g / 255
+	b = b / 255
+	max := ternaryMax(r, g, b)
+	min := ternaryMin(r, g, b)
+	if (min == 0) {
+		return float64(0)
+	}
+	if ((max - min) == 0) {
+		return float64(0)
+	}
+	var hue float64
+	if (max == r) {
+		hue = (g - b) / min
+	} else if (max == g) {
+		hue = 2.0 + (b - r) / (max - min)
+	} else {
+		hue = 4.0 + (r - g) / (max - min)
+	}
+	return hue
+}
+
+
+func createColor(str string) Color {
+	r, g, b := hexToRGB(str)
+	hue := hueFromRGB(r, g, b)
+	c := Color{Hex: str, R: r, G: g, B: b, Hue: hue}
+	return c
+}
+
+func createLangStruct(name string, hex string) Language {
+	color := createColor(hex)
+	l := Language{Name: name, Color: color}
+	return l
 }
 
 func main() {
@@ -64,18 +135,23 @@ func main() {
 	check(err)
 
 	// Convert map to JSON
-	// colorMap := make(map[string] *Language)
-	colorMap := make(map[string] string)
+	colorMap := make(map[string] Language)
 
 	for k, v := range yamlMap {
 		if val, ok := v["color"]; ok { // color exists
 			if str, ok := val.(string); ok { // string type check (required)
-				colorMap[k] = str// createLangStruct(k, str)
+				colorMap[k] =  createLangStruct(k, str)
 			}
 		}
 	}
 
-	colorJSON, err := json.MarshalIndent(colorMap, "", "    ")
+	languages := make(Languages, 0, len(colorMap))
+	for k := range colorMap {
+		languages = append(languages, colorMap[k])
+	}
+	sort.Sort(languages)
+
+	colorJSON, err := json.MarshalIndent(languages, "", "    ")
 	check(err)
 	allJSON, err := json.MarshalIndent(yamlMap, "", "    ")
 	check(err)
