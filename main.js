b26b508cb782220227997954c4d1386d247b8bcb
Zach Munro-Cape <zach.munrocape@gmail.com>
2015-03-01 22:05:11 -0400

    Refactored js

--- main.js
+++ main.js
@@ -2,9 +2,7 @@ $(document).ready(function(e) {
 	index = 0;
 	$.each(color_data, function(key, value) {
 		console.log(key, value);
-		row = $('<div>').text(value.Name);
-		row.css('backgroundColor', value.Color.Hex);
-		row.addClass('language');
+		row = $('<div>').text(value.Name).addClass('language').css('backgroundColor', value.Color.Hex);
 		$('#color-list').append(row);
 	});
 });
